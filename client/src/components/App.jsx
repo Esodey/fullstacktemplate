53eb6d969d061b0922ddf2be579d23abe380608b
Erik Soderholm <esodey@gmail.com>
2019-05-03 16:04:29 -0700

    Start to create a movie list with a given fullstack template. Completed step 0. Working on step 1.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import MovieList from './MovieList.js';
+import Search from './Search.js';
+import exampleMovies from '../../../data/exampleMovies.js';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movies : exampleMovies
+    };
   }
 
   render() {
     return (
       <div>
-        <h1 className="title">MERN-Stack Template</h1>
+        <h1 className="title">Movie List!</h1>
         <br />
-        <p>Happy coding!</p>
+        <div>
+          <div>
+            <Search movies={this.state.movies}/>
+          </div>
+          <MovieList movies={this.state.movies} /> 
+        </div>
       </div>
     );
   }
